2def1d9ce6266600329960204ec6f5e4d0ef923f
Alexander Sanchez <alexanderwsanchez1991@gmail.com>
2021-05-25 16:41:53 -0500

    Push code for Jack.

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,19 @@
 // require basic
-const Express = require('express');
-const Router = Express.Router();
-const Moment = require('moment-timezone');
-const Dedent = require('dedent-js');
-const Spawn = require('child_process').spawn;
+const express = require('express');
+const router = express.Router();
+const moment = require('moment-timezone');
+const dedent = require('dedent-js');
+const spawn = require('child_process').spawn;
 const { formatArguments, isAuthorized, unauthrizedMessage } = require('../helperFunctions');
 
 // require models
-const Nodes = require('../models/nodes');
-const PerfMons = require('../models/perfMons');
-const Videos = require('../models/videos');
+const nodes = require('../models/nodes');
+const perfMons = require('../models/perfMons');
+const videos = require('../models/videos');
 
-Router.post('/newNode', async (req, res) => {
+router.post('/newNode', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    const newNode = new Nodes({
+    const newNode = new nodes({
       name: req.body.name,
       ip: req.body.zeroTierIP,
       config: {
@@ -75,38 +75,30 @@ Router.post('/newNode', async (req, res) => {
   }
 });
 
-Router.post('/upDateNode/:nodeName', async (req, res) => {
+router.post('/upDateNode/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    Nodes.findOneAndUpdate(
-      { name: req.params.nodeName },
-      { $set: { lastCheckIn: new Date(), sysInfo: req.body } }
-    ).exec(function (err, node) {
-      if (err) {
-        console.log(err);
-        res.status(500).send(err);
-      } else {
-        res.status(200).send(node);
-      }
-    });
+    nodes
+      .findOneAndUpdate({ name: req.params.nodeName }, { $set: { lastCheckIn: new Date(), sysInfo: req.body } })
+      .exec(function (err, node) {
+        if (err) {
+          console.log(err);
+          res.status(500).send(err);
+        } else {
+          res.status(200).send(node);
+        }
+      });
   } else {
     res.json(unauthrizedMessage());
   }
 });
 
-Router.get('/nodes', async (req, res) => {
+router.get('/nodes', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    Nodes.find({}, function (err, docs) {
+    nodes.find({}, function (err, docs) {
       if (err) {
         console.log(err);
       } else {
         var response = [];
-        //for (i = 0; i < docs.length; i++) {
-        //  let hours = Moment().diff(Moment(docs[i].lastCheckIn), 'hours', true);
-        //  hours = hours.toFixed(2);
-        //
-        // if (hours < 0.3) {
-        //   response.push(docs[i]);
-        // }
       }
       res.send(docs);
     });
@@ -115,9 +107,9 @@ Router.get('/nodes', async (req, res) => {
   }
 });
 
-Router.get('/nodes/:nodeName', async (req, res) => {
+router.get('/nodes/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    Nodes.findOne({ name: req.params.nodeName }, function (err, doc) {
+    nodes.findOne({ name: req.params.nodeName }, function (err, doc) {
       if (err) {
         console.log(err);
       } else {
@@ -129,9 +121,10 @@ Router.get('/nodes/:nodeName', async (req, res) => {
   }
 });
 
-Router.get('/perfmons/:nodeName', async (req, res) => {
+router.get('/perfmons/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    PerfMons.find({ node: Nodes.findOne({ name: req.params.node })._id })
+    perfMons
+      .find({ node: nodes.findOne({ name: req.params.node })._id })
       .sort([['dateTime', 1]])
       .exec(function (err, docs) {
         if (err) {
@@ -145,10 +138,10 @@ Router.get('/perfmons/:nodeName', async (req, res) => {
   }
 });
 
-Router.post('/perfmons/:nodeName', async (req, res) => {
+router.post('/perfmons/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    const PerfMon = new PerfMons(req.params);
-    PerfMon.node = Nodes.findOne({ name: req.params.nodeName })._id;
+    const PerfMon = new perfMons(req.params);
+    PerfMon.node = nodes.findOne({ name: req.params.nodeName })._id;
     await PerfMon.save();
     res.send(PerfMon);
   } else {
@@ -156,9 +149,9 @@ Router.post('/perfmons/:nodeName', async (req, res) => {
   }
 });
 
-Router.get('/videos/:nodeName', async (req, res) => {
+router.get('/videos/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    Videos.find({ node: req.params.nodeName }, function (err, docs) {
+    videos.find({ node: req.params.nodeName }, function (err, docs) {
       if (err) {
         console.log(err);
       } else {
@@ -170,23 +163,24 @@ Router.get('/videos/:nodeName', async (req, res) => {
   }
 });
 
-Router.get('/videos/:date/:nodeName', async (req, res) => {
+router.get('/videos/:date/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
     const date = req.params.date;
 
     var documents = {
-      cam1: [],
-      cam2: [],
-      cam3: [],
+      camera1: [],
+      camera2: [],
+      camera3: [],
     };
 
-    Videos.find({
-      node: Nodes.findOne({ name: req.params.nodeName })._id,
-      dateTime: {
-        $gte: new Date(date + 'T00:00:00.000Z'),
-        $lte: new Date(date + 'T23:59:00.000Z'),
-      },
-    })
+    videos
+      .find({
+        node: nodes.findOne({ name: req.params.nodeName })._id,
+        dateTime: {
+          $gte: new Date(date + 'T00:00:00.000Z'),
+          $lte: new Date(date + 'T23:59:00.000Z'),
+        },
+      })
       .sort([['dateTime', 1]])
       .exec(function (err, docs) {
         if (err) {
@@ -194,14 +188,14 @@ Router.get('/videos/:date/:nodeName', async (req, res) => {
         } else {
           for (var i = 0; i < docs.length; i++) {
             switch (docs[i].camera) {
-              case 'cam1':
-                documents.cam1.push(docs[i]);
+              case 'camera1':
+                documents.camera1.push(docs[i]);
                 break;
-              case 'cam2':
-                documents.cam2.push(docs[i]);
+              case 'camera2':
+                documents.camera2.push(docs[i]);
                 break;
-              case 'cam3':
-                documents.cam3.push(docs[i]);
+              case 'camera3':
+                documents.camera3.push(docs[i]);
                 break;
             }
           }
@@ -213,7 +207,7 @@ Router.get('/videos/:date/:nodeName', async (req, res) => {
   }
 });
 
-Router.get('/videos/:startDate/:endDate/:nodeName', async (req, res) => {
+router.get('/videos/:startDate/:endDate/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
     const startDate = req.params.startDate;
     const splitFileString = startDate.split('_');
@@ -223,7 +217,7 @@ Router.get('/videos/:startDate/:endDate/:nodeName', async (req, res) => {
     const fileTime = fileTimesplit[0];
     const fileTimeCelaned = fileTime.split('-');
     const dateTime = fileData + ' ' + fileTimeCelaned[0] + ':' + fileTimeCelaned[1] + ':00';
-    const dateTimeString = Moment(dateTime).toISOString();
+    const dateTimeString = moment(dateTime).toISOString();
     const endDate = req.params.endDate;
     const splitFileString2 = endDate.split('_');
     const fileData2 = splitFileString2[0];
@@ -232,13 +226,13 @@ Router.get('/videos/:startDate/:endDate/:nodeName', async (req, res) => {
     const fileTime2 = fileTimesplit2[0];
     const fileTimeCelaned2 = fileTime2.split('-');
     const dateTime2 = fileData2 + ' ' + fileTimeCelaned2[0] + ':' + fileTimeCelaned2[1] + ':00';
-    const dateTimeString2 = Moment(dateTime2);
-    const time = Moment.duration('00:04:00');
+    const dateTimeString2 = moment(dateTime2);
+    const time = moment.duration('00:04:00');
     const dateTimeString2Cleaned = dateTimeString2.subtract(time).toISOString();
 
-    Videos.find(
+    videos.find(
       {
-        node: Nodes.findOne({ name: req.params.nodeName })._id,
+        node: nodes.findOne({ name: req.params.nodeName })._id,
         dateTime: {
           $gte: dateTimeString,
           $lte: dateTimeString2Cleaned,
@@ -257,9 +251,9 @@ Router.get('/videos/:startDate/:endDate/:nodeName', async (req, res) => {
   }
 });
 
-Router.get('/videos/dates/:nodeName', async (req, res) => {
+router.get('/videos/dates/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    Videos.find({ node: req.params.nodeName }, { DateTime: true, _id: false }, function (err, docs) {
+    videos.find({ node: req.params.nodeName }, { DateTime: true, _id: false }, function (err, docs) {
       if (err) {
         console.log(err);
       } else {
@@ -272,9 +266,10 @@ Router.get('/videos/dates/:nodeName', async (req, res) => {
   }
 });
 
-Router.get('/videos/oldest/:nodeName', async (req, res) => {
+router.get('/videos/oldest/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    Videos.findOne({ node: req.params.nodeName })
+    videos
+      .findOne({ node: req.params.nodeName })
       .sort({ date: -1 })
       .exec(function (err, docs) {
         res.send(docs);
@@ -284,10 +279,10 @@ Router.get('/videos/oldest/:nodeName', async (req, res) => {
   }
 });
 
-Router.post('/videos/create', async (req, res) => {
+router.post('/videos/create', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    const video = await new Videos({
-      node: Nodes.findOne({ name: req.body.nodeName })._id,
+    const video = await new videos({
+      node: nodes.findOne({ name: req.body.nodeName })._id,
       fileLocation: req.body.filename,
       location: { lat: req.body.location.lat, lng: req.body.location.lng },
       startPts: req.body.start_pts,
@@ -306,19 +301,19 @@ Router.post('/videos/create', async (req, res) => {
   }
 });
 
-Router.get('/streams/start/:nodeName/:cameraIP', async (req, res) => {
+router.get('/streams/start/:nodeName/:cameraIP', async (req, res) => {
   if (isAuthorized(req.query.token)) {
     var nodeName = req.params.nodeName;
     var cameraIP = req.params.cameraIP;
 
     streamingCamerasOBJ[nodeName] = {};
-    streamingCamerasOBJ[nodeName]['cam1'] = null;
-    streamingCamerasOBJ[nodeName]['cam2'] = null;
-    streamingCamerasOBJ[nodeName]['cam3'] = null;
+    streamingCamerasOBJ[nodeName]['camera1'] = null;
+    streamingCamerasOBJ[nodeName]['camera2'] = null;
+    streamingCamerasOBJ[nodeName]['camera3'] = null;
 
-    streamingCamerasOBJ[nodeName].cam1 = Spawn(
+    streamingCamerasOBJ[nodeName].camera1 = spawn(
       'ffmpeg',
-      Dedent`
+      dedent`
       -rtsp_transport 
       tcp 
       -i 
@@ -327,13 +322,13 @@ Router.get('/streams/start/:nodeName/:cameraIP', async (req, res) => {
       copy 
       -f 
       flv 
-      rtmp://10.10.10.53/${nodeName}/camera1
+      rtmp://10.10.10.53/${nodeName}/camera1?sign=9999999999${md5('/' + nodeName + '/camera1-9999999999-UUnv9njxg')}
     `
     );
 
-    streamingCamerasOBJ[nodeName].cam2 = Spawn(
+    streamingCamerasOBJ[nodeName].camera2 = spawn(
       'ffmpeg',
-      Dedent`
+      dedent`
       -rtsp_transport 
       tcp 
       -i 
@@ -342,13 +337,13 @@ Router.get('/streams/start/:nodeName/:cameraIP', async (req, res) => {
       copy 
       -f 
       flv 
-      rtmp://10.10.10.53/${nodeName}/camera2
+      rtmp://10.10.10.53/${nodeName}/camera2?sign=9999999999${md5('/' + nodeName + '/camera2-9999999999-UUnv9njxg')}
     `
     );
 
-    streamingCamerasOBJ[nodeName].cam3 = Spawn(
+    streamingCamerasOBJ[nodeName].camera3 = spawn(
       'ffmpeg',
-      Dedent`
+      dedent`
       -rtsp_transport 
       tcp 
       -i 
@@ -357,7 +352,7 @@ Router.get('/streams/start/:nodeName/:cameraIP', async (req, res) => {
       copy 
       -f 
       flv 
-      rtmp://10.10.10.53/${nodeName}/camera3
+      rtmp://10.10.10.53/${nodeName}/camera3?sign=9999999999${md5('/' + nodeName + '/camera3-9999999999-UUnv9njxg')}
     `
     );
 
@@ -367,15 +362,15 @@ Router.get('/streams/start/:nodeName/:cameraIP', async (req, res) => {
   }
 });
 
-Router.get('/streams/stop/:nodeName', async (req, res) => {
+router.get('/streams/stop/:nodeName', async (req, res) => {
   if (isAuthorized(req.query.token)) {
-    streamingCamerasOBJ[req.params.nodeName]['cam1'].stdin.write('q');
-    streamingCamerasOBJ[req.params.nodeName]['cam2'].stdin.write('q');
-    streamingCamerasOBJ[req.params.nodeName]['cam3'].stdin.write('q');
+    streamingCamerasOBJ[req.params.nodeName]['camera1'].stdin.write('q');
+    streamingCamerasOBJ[req.params.nodeName]['camera2'].stdin.write('q');
+    streamingCamerasOBJ[req.params.nodeName]['camera3'].stdin.write('q');
     res.send('ok');
   } else {
     res.json(unauthrizedMessage());
   }
 });
 
-module.exports = Router;
+module.exports = router;
