c70b34e08327cf8e47fbf63572afb26ceedea652
Alexander Sanchez <alexanderwsanchez1991@gmail.com>
2021-06-04 19:35:38 -0500

    Format code.

--- models/servers.js
+++ models/servers.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 const schema = mongoose.Schema({
   name: { type: String, default: '********' },
-  service: {type: String, default: '********'},
+  service: { type: String, default: '********' },
   zeroTierNetworkID: { type: String, default: '' },
   zeroTierIP: { type: String, default: '' },
-// Default properties.
+  // Default properties.
   createdAt: { type: Date, default: Date.now },
   updatedAt: Date,
 });
