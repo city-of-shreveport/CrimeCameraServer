d447d43393ddc4bf012c296f68376a8bce27a2cf
Alexander Sanchez <alexanderwsanchez1991@gmail.com>
2021-07-13 20:43:55 -0500

    Checkpoint.

--- routes/perfMons.js
+++ routes/perfMons.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var perfMons = require('../models/perfMons');
 const { spawn } = require('child_process');
-var nodes = require('../models/nodes')
+var nodes = require('../models/nodes');
 var { formatArguments } = require('../helperFunctions');
 const fetch = require('node-fetch');
 router.get('/', async (req, res) => {
@@ -17,47 +17,44 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  let nodePerfmon = req.body
-  let nmapScan = null
-  nodePerfmon.cameraStatus = {}
+  let nodePerfmon = req.body;
+  let nmapScan = null;
+  nodePerfmon.cameraStatus = {};
   nodes.findOne({ name: req.body.node }, function (err, doc) {
-    if(doc){
-    nmapScan = spawn(
-    'nmap',  ['-p', '554-556', doc.config.ip]
-    );
-   
-    nmapScan.stdout.on('data', (data) => {
-    
-      var dataStringSplit = data.toString().split("\n")
-      for(i=0;i<dataStringSplit.length;i++){
-        if(dataStringSplit[i].includes('554') && dataStringSplit[i].includes('open')){
-          nodePerfmon.cameraStatus.camera1 = true
-        }
-        if(dataStringSplit[i].includes('555') && dataStringSplit[i].includes('open')){
-            nodePerfmon.cameraStatus.camera2 = true
-        }
-        if(dataStringSplit[i].includes('556') && dataStringSplit[i].includes('open')){
-            nodePerfmon.cameraStatus.camera3 = true
-        }
-        if(dataStringSplit[i].includes('554') && dataStringSplit[i].includes('filtered')){
-          nodePerfmon.cameraStatus.camera1 = false
-        }
-        if(dataStringSplit[i].includes('555') && dataStringSplit[i].includes('filtered')){
-            nodePerfmon.cameraStatus.camera2 = false
-        }
-        if(dataStringSplit[i].includes('556') && dataStringSplit[i].includes('filtered')){
-            nodePerfmon.cameraStatus.camera3 = false
-        }   
-      }
+    if (doc) {
+      nmapScan = spawn('nmap', ['-p', '554-556', doc.config.ip]);
 
+      nmapScan.stdout.on('data', (data) => {
+        var dataStringSplit = data.toString().split('\n');
+        for (i = 0; i < dataStringSplit.length; i++) {
+          if (dataStringSplit[i].includes('554') && dataStringSplit[i].includes('open')) {
+            nodePerfmon.cameraStatus.camera1 = true;
+          }
+          if (dataStringSplit[i].includes('555') && dataStringSplit[i].includes('open')) {
+            nodePerfmon.cameraStatus.camera2 = true;
+          }
+          if (dataStringSplit[i].includes('556') && dataStringSplit[i].includes('open')) {
+            nodePerfmon.cameraStatus.camera3 = true;
+          }
+          if (dataStringSplit[i].includes('554') && dataStringSplit[i].includes('filtered')) {
+            nodePerfmon.cameraStatus.camera1 = false;
+          }
+          if (dataStringSplit[i].includes('555') && dataStringSplit[i].includes('filtered')) {
+            nodePerfmon.cameraStatus.camera2 = false;
+          }
+          if (dataStringSplit[i].includes('556') && dataStringSplit[i].includes('filtered')) {
+            nodePerfmon.cameraStatus.camera3 = false;
+          }
+        }
+
+        new perfMons(nodePerfmon).save();
+        res.end();
+      });
+    } else {
       new perfMons(nodePerfmon).save();
       res.end();
-    });
-}else{new perfMons(nodePerfmon).save();
-      res.end();
-}
-  
-})
+    }
+  });
 });
 
 router.get('/:nodeName', async (req, res) => {
@@ -72,10 +69,5 @@ router.get('/:nodeName', async (req, res) => {
       }
     });
 });
-
-
-
-
- 
 
 module.exports = router;
