56d05d3ea74184fb6fdf398c5af537ce98ab3fd3
Alexander Sanchez <alexanderwsanchez1991@gmail.com>
2021-08-11 14:53:09 -0500

    Checkpoint.

--- bin/www.js
+++ bin/www.js
@@ -5,9 +5,10 @@
 var app = require('../app');
 var debug = require('debug')('CrimeCameraServer:server');
 var http = require('http');
+var nodeMediaServer = require('node-media-server');
 var fetch = require('node-fetch');
 var streamMons = require('../models/streamMons.js');
-var { formatArguments, tryValue, cleanupVideos, mountNodes } = require('../helperFunctions');
+var { formatArguments, tryValue, cleanupVideos } = require('../helperFunctions');
 
 /**
  * Get port from environment and store in Express.
@@ -36,11 +37,48 @@ setInterval(() => {
 
 cleanupVideos();
 
-setInterval(() => {
-  mountNodes();
-}, 900000);
+let tasks = [];
 
-mountNodes();
+function retreiveNodesList() {
+  fetch('http://localhost:3000/api/nodes')
+    .then((response) => response.json())
+    .then((json) => {
+      json.map((node) => {
+        tasks.push({
+          app: node.name,
+          mode: 'pull',
+          edge: 'rtmp://' + node.config.ip,
+        });
+      });
+    });
+}
+
+const config = {
+  rtmp: {
+    port: 1936,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60,
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*',
+  },
+
+  relay: {
+    ffmpeg: '/usr/bin/ffmpeg',
+    tasks: tasks,
+  },
+};
+
+var nms = new nodeMediaServer(config);
+setTimeout(() => {
+  retreiveNodesList();
+  setTimeout(() => {
+    nms.run();
+  }, 6000);
+}, 4000);
 
 /**
  * Normalize a port into a number, string, or false.
