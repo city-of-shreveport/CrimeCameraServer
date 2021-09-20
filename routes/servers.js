4a0147fbeda2d19429584fa780973130135fe8f4
Alexander Sanchez <alexanderwsanchez1991@gmail.com>
2021-09-20 07:19:20 -0500

    Finish up 1.0.1 changes.

--- routes/servers.js
+++ routes/servers.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var servers = require('../models/servers');
+var { exec } = require('child_process');
 
 router.get('/', async (req, res) => {
   servers.find({}, function (err, docs) {
@@ -15,14 +16,34 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   var newServer = new servers({
     name: req.body.name,
-    service: req.body.service,
-    zeroTierIP: req.body.zeroTierIP,
-    zeroTierNetworkID: req.body.zeroTierNetworkID,
-    lastCheckIn: new Date(),
+    type: req.body.type,
+    ip: req.body.ip,
   });
-  console.log(newServer);
+
   await newServer.save();
   res.send(newServer);
+});
+
+router.post('/delete', async (req, res) => {
+  await servers.findOneAndDelete({ _id: req.body._id }, function (err, docs) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(docs);
+    }
+  });
+});
+
+router.post('/reboot', async (req, res) => {
+  var server = await servers.findOne({ _id: req.body._id }, function (err, docs) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(docs);
+    }
+  });
+
+  exec(`ssh pi@${server.ip} 'sudo reboot now'`, (error, stdout, stderr) => {});
 });
 
 module.exports = router;
